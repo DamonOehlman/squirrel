c5a8ddf9002ae26f335a204512f50d962333dc91
Damon Oehlman <damon.oehlman@sidelab.com>
2012-08-13 16:54:25 +1000

    Fixed existing module test to use underscore instead of pkginfo

--- test/existing.js
+++ test/existing.js
@@ -1,18 +1,18 @@
 var squirrel = require('../'),
-    expect = require('expect.js');
+    assert = require('assert');
     
 describe('require existing modules check', function() {
     it('should be able to successfully include a module that exists already', function(done) {
         squirrel('read', function(err, read) {
-            expect(read).to.be.ok();
+            assert(read);
             done(err);
         });
     });
     
     it('should be able to include multiple modules in a single call', function(done) {
-        squirrel(['read', 'pkginfo'], function(err, read, pkginfo) {
-            expect(read).to.be.ok();
-            expect(pkginfo).to.be.ok();
+        squirrel(['read', 'underscore'], function(err, read, _) {
+            assert(read);
+            assert(_);
             done(err);
         });
     });
